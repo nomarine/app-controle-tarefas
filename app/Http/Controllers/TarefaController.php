6f2368bbffbfb848ff6e746fbd147320daa2b392
nomarine <rafaelfijos@gmail.com>
2023-11-28 20:12:06 -0300

    Implement auth middleware to tarefa routes

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -12,9 +12,14 @@ class TarefaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
-        //
+        return "Chegamos aqui!";
     }
 
     /**
