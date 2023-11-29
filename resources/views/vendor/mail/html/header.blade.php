45f42b8f3a74eb39e13ad32a4281fdc1f4a733a3
nomarine <rafaelfijos@gmail.com>
2023-11-29 10:41:04 -0300

    Change logo source to Github repo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Controle de Tarefas')
-<img src="{{ asset('img/logo.png')}}" class="logo" alt="Controle de Tarefas Logo">
+<img src="https://raw.githubusercontent.com/nomarine/app-controle-tarefas/smtp-config/public/img/logo.png" class="logo" alt="Controle de Tarefas Logo">
 @else
 {{ $slot }}
 @endif
