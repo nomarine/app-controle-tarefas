964b016c01c936c7e0a5392c74d96ab62f62262e
nomarine <rafaelfijos@gmail.com>
2023-11-29 10:38:48 -0300

    Add logo image

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Controle de Tarefas')
-<img src="http://localhost:8000/img/logo.png" class="logo" alt="Controle de Tarefas Logo">
+<img src="{{ asset('img/logo.png')}}" class="logo" alt="Controle de Tarefas Logo">
 @else
 {{ $slot }}
 @endif
