7cf6b078140d8afcab7e33832ff40923d27f14f0
nomarine <rafaelfijos@gmail.com>
2023-12-12 17:16:50 -0300

    Improve navbar; add tarefas index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <!-- <a class="navbar-brand" href="{{ url('/') }}"> -->
+                <a class="navbar-brand" href="{{ route('tarefa.index') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -52,6 +53,10 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" href="{{ route('tarefa.index') }}">Tarefas</a>
+                            </li>
+                                                       
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
--- resources/views/tarefa/index.blade.php
+++ resources/views/tarefa/index.blade.php
@@ -5,7 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Tarefas</div>
+                <div class="card-header">
+                  Tarefas
+                  <a class="float-right" href="{{ route('tarefa.create') }}">Novo</a>
+                </div>
 
                 <div class="card-body">
                   <table class="table">
